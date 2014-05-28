f43e179a9f13ccfe7b38e40b3b0d3ee20af6cfb5
Gord Tanner <gtanner@gmail.com>
2014-05-28 09:54:19 -0400

    renamed zmq sample to fourism

---
+++ bin/fourism.js
@@ -0,0 +1,120 @@
+#!/usr/bin/env node
+var zmq = require('zmq'),
+    async = require('async'),
+    cluster = require('cluster'),
+    utils = require('./../lib/utils');
+    push = zmq.socket('push'),
+    pull = zmq.socket('pull');
+
+var program = require('commander');
+var prompt = require('prompt');
+
+program
+  .version(require('./../package.json').version);
+
+program
+  .command('master')
+  .description('Zuul, the Gatekeeper')
+  .action(function () {
+    push.bindSync('tcp://*:1337');
+    pull.bindSync('tcp://*:1338');
+
+    var workers = 0, callbacks = [];
+
+    pull.on('message', function (msg, data) {
+      msg = msg.toString();
+      data = JSON.parse(data.toString());
+
+      switch (msg) {
+        case 'result':
+          var cb = callbacks.pop();
+          return cb && cb(null, data);
+          break;
+        default:
+          workers++;
+          console.log('worker connected,', workers, 'are in the house');
+      }
+    });
+
+    function send(range, callback) {
+      callbacks.push(callback);
+      push.send([range.start, range.end]);
+    }
+
+    function handle(msg) {
+      utils.log(msg.start, msg.end, msg.result);
+    }
+    
+    prompt.start();
+
+    prompt.get({
+      name: 'millions',
+      type: 'number',
+      message: 'how many millions?!?!?'
+    }, function (err, args) {
+      var start = Date.now(), batches = [];
+
+      for (var x = 0; x < args.millions; x++) {
+        batches.push(async.apply(send, utils.range(x)));
+      }
+
+      async.parallel(batches, function (err, results) {
+        results.forEach(handle);
+        console.log('time:', (Date.now() - start) / 1000);
+        process.exit();
+      });
+    });
+  });
+
+
+program
+  .command('slave [ip]')
+  .description('Vinz Clortho, Keymaster of Gozer')
+  .action(function (ip) {
+    if (cluster.isMaster) {
+      async.times(4, cluster.fork);
+
+      prompt.start();
+      prompt.message = '';
+      prompt.delimiter = '';
+
+      prompt.get({name: 'exit', message: '\n'}, process.exit);
+      console.log('\npress enter to exit');
+    }
+    else {
+      ip = ip || 'localhost';
+      console.log('worker process', process.pid, 'connected to', ip);
+      pull.connect('tcp://' + ip + ':1337');
+      push.connect('tcp://' + ip + ':1338');
+
+      var wordify = require('./../lib/wordify'),
+          fourism = require('./../lib/4our');
+
+
+      pull.on('message', function (start, end) {
+        console.log('the fours is with you:', start.toString(), end.toString());
+        start = parseInt(start.toString(), 10);
+        end = parseInt(end.toString(), 10);
+
+        var counts = {};
+
+        for (var x = start; x < end; x++) {
+          var turns = fourism(wordify(x));
+          counts[turns] = ++counts[turns] || 1;
+        }
+
+        push.send(['result', JSON.stringify({
+          start: start, 
+          end: end, 
+          result: counts
+        })]);
+      });
+
+      setTimeout(function () { push.send(['hello', 3]); }, 10);
+    }
+  });
+
+program.parse(process.argv);
+if (program.args.length === 0) {
+  program.help();
+}
